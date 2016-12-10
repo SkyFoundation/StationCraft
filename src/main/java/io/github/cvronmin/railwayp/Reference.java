9e1d29f5855ceb6ae113560384f79968eec36e5e
cvronmin <lung1a16@hotmail.com>
2016-12-10 18:11:54 +0800

    prepare for self-version-string-update

--- src/main/java/io/github/cvronmin/railwayp/Reference.java
+++ src/main/java/io/github/cvronmin/railwayp/Reference.java
@@ -3,7 +3,7 @@ package io.github.cvronmin.railwayp;
 public class Reference {
 	public static final String MODID = "railwayp";
 	public static final String NAME = "StatCraft";
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "@version@";
 	public static final String CPROXY = "io.github.cvronmin.railwayp.client" + ".ClientProxy";
 	public static final String SPROXY = "io.github.cvronmin.railwayp" + ".CommonProxy";
 }
